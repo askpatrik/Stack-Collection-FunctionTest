739713edd77896c1ec49f16d0f9729dc918703cc
Patrik Ask <110780495+askpatrik@users.noreply.github.com>
2023-01-09 14:33:56 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+namespace StackStuff
+{
+    public class StackList
+    {
+        public Stack myStack = new Stack();        
+        public void PushNumber(int number)
+        {           
+            myStack.Push(number);              
+        }
+        public void PushXNumbers(params int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)           
+                myStack.Push(list[i]);           
+        }
+        public void ClearTheStack()
+        {
+            myStack.Clear();
+        }
+        public string ContainsCertainNumber(int no1)
+        {
+            if (!myStack.Contains(no1))
+                return "Does not contain number";
+            else           
+                return no1.ToString();           
+        }
+        
+    }
+}
